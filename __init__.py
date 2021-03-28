71dcc09ba7a98834bf4f9e8251c6b4af49709be8
Thomas <torze001@plattsburgh.edu>
2021-03-28 18:57:09 -0400

    after login the website now redirects to googlemaps.html

--- __init__.py
+++ __init__.py
@@ -35,4 +35,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import maps
+    app.register_blueprint(maps.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
