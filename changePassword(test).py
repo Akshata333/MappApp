d4b05ce3adb5d93c69300a58121bd41e80f36a14
yxiao002 <79670264+yxiao002@users.noreply.github.com>
2021-03-28 01:46:35 -0400

    Update changePassword(test).py
    changePassword(test)

---
+++ changePassword(test).py
@@ -0,0 +1,6 @@
+class ChangePasswordForm(Form):
+    old_password = PasswordField('Old password', validators=[Required()])
+    password = PasswordField('New password', validators=[
+        Required(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm new password', validators=[Required()])
+    submit = SubmitField('Update Password')
